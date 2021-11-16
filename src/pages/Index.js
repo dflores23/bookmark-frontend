84103d10e5e9a7744f33445d3d2868a1b08ee2f6
craig-mcgowan <craigmcgowan15@gmail.com>
2021-11-16 15:44:26 -0500

    updates Main.js and Index.js to add delete functionality to Index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,9 +1,19 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import Form from "../components/Form"
 
 
 function Index(props) {
+    const navigate = useNavigate()
+    
+
+    const removeBookmark = (e) => {
+        props.deleteBookmark(e.target.id)
+        navigate("/")
+    }
+
+    
+
     const loaded = () => (
         props.bookmarks.map((bookmark) => {
             const { _id, title, url } = bookmark
@@ -11,6 +21,7 @@ function Index(props) {
                 <div key={_id} className="bookmark">
                     <a href={url}> <p>{title}</p></a>
                     <Link to={`/${_id}`}> <button className="update-btn">Update Bookmark</button></Link>
+                    <button id={_id} onClick={(e)=>removeBookmark(e)}>x</button>
 
                 </div>
             )
