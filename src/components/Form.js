e5e1ae10f92056ccbf1f85fe4e2199de4daa5c93
craig-mcgowan <craigmcgowan15@gmail.com>
2021-11-16 15:01:51 -0500

    updates Main.js Index.js App.js and App.js

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+
+const Form = props => {
+  return (
+  <form>
+    <h3>Form</h3>
+  </form>
+  )
+}
+
+export default Form
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,37 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import Form from "../components/Form"
+
+
 function Index(props) {
-    return <h1>Index</h1>
+    const loaded = () => (
+        props.bookmarks.map((bookmark) => {
+            const { _id, title, url } = bookmark
+            return (
+                <div key={_id} className="bookmark">
+                    <a href={url}> <p>{title}</p></a>
+                    <Link to={`/${_id}`}> <button className="update-btn">Update Bookmark</button></Link>
+
+                </div>
+            )
+        }
+        ))
+    
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    }   
+
+
+    return (
+        <>
+            <Form />
+            {props.bookmarks ? loaded() : loading()}
+            
+            
+        </>
+
+    )
+    
 }
 
 
